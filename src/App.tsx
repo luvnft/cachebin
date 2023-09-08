5bd157798c6d37ae1bf4989024e7006c8f518fc7
suyashpatil78 <suyash.p@fyle.in>
2023-09-08 21:39:44 +0530

    headers and description of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Block } from 'baseui/block';
 
 const App = (): JSX.Element => {
-  return <div>App</div>;
+  return (
+    <Block style={{ padding: '20px' }}>
+      <h1>Cachebin</h1>
+      <h4>
+        Cachebin is a safe and decentralized service to share code snippets, notes and many more.
+        Paste anything and share with your friends
+      </h4>
+    </Block>
+  );
 };
 
 export default App;
