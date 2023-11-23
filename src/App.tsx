e75ba0d178494fc0e3bbb48090f94c07ce8e051c
luv nft <info@blkluv.org>
2023-11-23 07:29:09 -0500

    paste emoji

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Tabs from './components/tabs/Tabs';
 const App = (): JSX.Element => {
   return (
     <Block className="page--header">
-      <h1>Paste</h1>
+      <h1>📋 Paste</h1>
       <h4>
         Paste is a secure and decentralized service for sharing code snippets, notes, and much
         more. Paste anything and share it with your friends. Paste it to me.
